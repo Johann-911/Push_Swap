8dacd5cd29b1c91d71d214b04858fddb5d87f138
Jannis Toumanidis <jtoumani@1-B-15.42heilbronn.de>
2025-09-05 11:53:14 +0200

    PushSwap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jtoumani <jtoumani@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 12:26:43 by jtoumani          #+#    #+#             */
-/*   Updated: 2025/06/23 12:21:00 by jtoumani         ###   ########.fr       */
+/*   Updated: 2025/06/23 17:23:01 by jtoumani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int	main(int argc, char **argv)
 	t_stack	*a;
 	t_stack	*b;
 
+	if (argc == 1)
+		return (1);
 	if (argc < 2 || (argc == 2 && argv[1][0] == '\0'))
 	{
 		write(2, "Error\n", 6);
@@ -44,5 +46,5 @@ int	main(int argc, char **argv)
 	return (free_stack(a), free_stack(b), 0);
 }
 
-	// print_stack(a, "A");
-	// print_stack(b, "B");
+// print_stack(a, "A");
+// print_stack(b, "B");
